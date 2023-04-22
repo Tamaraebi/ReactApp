3f535db17402d4da6f56f4d7139d8ef6e5a2862a
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-04-22 09:54:52 -0400

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-
 export default function About() {
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => {
-    const getContact = async () => {
-      let response = await fetch(
-        "http://laravel.tamaraebi.ca/api/contacts"
-      );
-      let data = await response.json();
-      //   console.log(data);
-      setContacts(data);
-    };
-    getContact();
-  }, []);
-
   return (
     <main id="main">
-      <h1 id="portfolio">Welcome to Tamara's portfolio!</h1>
-        <div id="contact_so">
-          {contacts.map((contact) => (
-            <div key={contact.id}>
-              <a href={contact.url}>
-                <h4>{contact.title}</h4>
-              </a>
-            </div>
-          ))}
-        </div>
-        <p id="intro">
+     <h1 id="portfolio">Welcome to Tamara's portfolio!</h1>
+     <p id="intro">
         I am a highly motivated and enthusiastic web development professional with a strong foundation in HTML, CSS, PHP, AWS and JavaScript 
         seeking to join a constantly challenging work environment that enables the application of my knowledge, skills and experiences, promotes
         my intellectual development and career growth towards achieving corporate goals and vision. I have Proficiency in developing responsive
