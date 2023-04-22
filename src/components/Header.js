69aa100cc413dc224d5725e4c52d273ecc3d6665
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-04-22 10:05:07 -0400

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
     return (
       <header id="header">
         <h1 id="site-name">
-          <a href="/">Tamara Ebi-ukuli</a>
+          <a href="/">Tamara Ebi-ukuli: Portfolio</a>
         </h1>
       </header>
     );
